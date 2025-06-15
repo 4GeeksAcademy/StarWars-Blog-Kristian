c1bd89642f7556781da2624a3f7f2b42f41f80d2
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-06-15 23:51:49 +0000

    Added delete functionality to remove favorites from dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,30 @@
 import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Navbar = () => {
-	const {store, dispatch} = useGlobalReducer()
-	
+	const { store, dispatch } = useGlobalReducer()
+	const handleDelete = (name) => {
+		const action = {
+			type: "remove_favorite",
+			payload: { removingFavorite: name }
+		}
+		dispatch(action)
+	}
+
 	return (
 		<nav className="navbar navbar-grey">
 			<div className="container">
 				<h3 className="black-font">Star Wars</h3>
 				<div className="ml-auto">
-					<div class="dropdown">
-						<button class="btn details-button" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-							My Favorites <i class="fa-solid fa-arrow-down"></i>
+					<div className="dropdown">
+						<button className="btn details-button" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							My Favorites <i className="fa-solid fa-arrow-down"></i>
 						</button>
-						<ul class="dropdown-menu dropdown-favorites">
+						<ul className="dropdown-menu dropdown-favorites">
 							{store.favorites.map((favorite, index) => (
-								<li key={index} className="dropdown-item dropdown-favorites dropdown-item-favorites">{favorite}</li>
+								<li key={index} className="dropdown-item dropdown-favorites dropdown-item-favorites">
+									<span className="favorite-name">{favorite}</span>
+									<button className="float-end btn details-button" onClick={() => handleDelete(favorite)}><i className="fa-solid fa-trash"></i></button>
+								</li>
 							))}
 						</ul>
 					</div>
