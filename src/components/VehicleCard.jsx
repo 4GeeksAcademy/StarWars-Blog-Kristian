ea5358edcb8179ada058de6ea908a9dd08cc203b
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-06-15 23:12:10 +0000

    Added favorites functionality to vehicle cards

--- src/components/VehicleCard.jsx
+++ src/components/VehicleCard.jsx
@@ -3,14 +3,32 @@ import useGlobalReducer from "../hooks/useGlobalReducer";
 import { useNavigate } from "react-router-dom";
 
 const VehicleCard = (props) => {
+    const { store, dispatch } = useGlobalReducer()
     const navigate = useNavigate()
+    const handleFavorite = () => {
+        const addedFavorite = store.favorites.includes(props.name)
+
+        if (addedFavorite === true) {
+            const action = {
+                type: "remove_favorite",
+                payload: { removingFavorite: props.name }
+            }
+            dispatch(action)
+        } else {
+            const action = {
+                type: "add_favorite",
+                payload: { newFavorite: props.name }
+            }
+            dispatch(action)
+        }
+    }
     return (
         <div className="card mx-2" style={{ minWidth: "18rem", background: "gray" }}>
             <img src={`https://raw.githubusercontent.com/breatheco-de/swapi-images/refs/heads/master/public/images/vehicles/${props.uid}.jpg`} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title black-font">{props.name}</h5>
                 <button className="btn details-button" onClick={() => { navigate(`/vehicle/details/${props.uid}`) }}>Details</button>
-                <button className="btn details-button m-2"><i class="fa-solid fa-heart"></i></button>
+                <button className="btn details-button m-2" onClick={handleFavorite}><i class="fa-solid fa-heart"></i></button>
             </div>
         </div>
     );
